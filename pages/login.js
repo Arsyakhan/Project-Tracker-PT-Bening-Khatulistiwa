19e9d09a913aec4a165e59fd204d9e12d8eb7896
Syafiq Khanafi Arifin <syafiqkhanafi.123@gmail.com>
2026-09-09 16:29:07 +0700

    Update login password validation
    Change login password from 'admin123' to 'BK123'.

--- pages/login.js
+++ pages/login.js
@@ -10,7 +10,7 @@ export default function Login() {
     e.preventDefault();
     
     // UBAH PASSWORD DI SINI
-    if (password === 'admin123') {
+    if (password === 'BK123') {
       // Set cookie penanda login yang berlaku selama 1 hari (86400 detik)
       document.cookie = "isLoggedIn=true; path=/; max-age=86400";
       router.push('/'); // Arahkan ke dashboard
