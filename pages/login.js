461255289bacaf17401730b10df1977b652a1cba
Syafiq Khanafi Arifin <syafiqkhanafi.123@gmail.com>
2026-09-14 15:44:00 +0700

    Change login password from 'admin123' to 'bening123'

--- pages/login.js
+++ pages/login.js
@@ -10,7 +10,7 @@ export default function Login() {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    if (password === 'admin123') {
+    if (password === 'bening123') {
       document.cookie = "isLoggedIn=true; path=/";
       router.push('/');
     } else {
